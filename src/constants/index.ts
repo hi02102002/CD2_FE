a6e79c482c6a7ddd4e8124f9762f6c822f3d5653
Son <vts27.06.02@gmail.com>
2023-02-20 19:31:50 +0700

    .

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,5 @@ export const ROUTES = {
     HOME: '/',
     ADMIN: '/admin',
     LOGIN: '/login',
+    REGISTER: '/register',
 };
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,61 @@
+// type Props = {};
+
+// const Register = (props: Props) => {
+//     return <div>Register</div>;
+// };
+
+// export default Register;
+
+import { PageTop, SignInSocial } from '@/components/common';
+import { LoginContainer } from '@/components/Login';
+import { ROUTES } from '@/constants';
+import { ClientLayout } from '@/layouts/client';
+import { NextPageWithLayout } from '@/types/shared';
+import { styled } from '@mui/material';
+
 type Props = {};
 
-const Register = (props: Props) => {
-    return <div>Register</div>;
+const Login: NextPageWithLayout<Props> = (props) => {
+    return (
+        <>
+            <PageTop
+                title="Create New Customer Account"
+                breadcrumbItems={[
+                    {
+                        href: ROUTES.HOME,
+                        name: 'Home',
+                    },
+                    {
+                        href: ROUTES.REGISTER,
+                        name: 'Register',
+                    },
+                ]}
+            />
+            <Main className="main">
+                <LoginP className="block-content">
+                    <SignInSocial social="facebook" />
+                    <SignInSocial social="google" />
+                </LoginP>
+                <LoginContainer></LoginContainer>
+            </Main>
+        </>
+    );
 };
 
-export default Register;
+Login.getLayout = (page) => {
+    return <ClientLayout>{page}</ClientLayout>;
+};
+
+const LoginP = styled('div')`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 48px;
+` 
+
+const Main = styled('div')`
+    max-width: 1200px;
+    margin: 40px auto 0px;
+` 
+
+export default Login;
+
