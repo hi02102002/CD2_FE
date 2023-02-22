66edd59e14bbf8d2cf30ccb1895292c37bb061e3
Son <vts27.06.02@gmail.com>
2023-02-22 21:05:50 +0700

    test

--- src/components/Login/LoginContainer/LoginContainer.tsx
+++ src/components/Login/LoginContainer/LoginContainer.tsx
@@ -12,7 +12,7 @@ function LoginContainer(){
                         <Text>If you have an account, sign in with your email address.</Text>
                         <LoginForm></LoginForm>
                     </Grid>
-    
+        
                     <Grid item className="Unregistered" sm={12} md={6} sx={{padding:'0 0 0 48px'}}>
                         <Typography variant="h5" fontWeight='600' mb='15px' lineHeight='42px'>Create an Account</Typography>
                         <Text>Sign up for early Sale access plus tailored new arrivals, trends and promotions. To opt out, click unsubscribe in our emails.</Text>
