f07791b3fe094c300803d0ac247614622c7ad04b
Võ Hoàng Huy <84460646+hi02102002@users.noreply.github.com>
2023-03-29 22:01:04 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,24 @@ const nextConfig = {
             'upload-image-shoppingcart.s3.ap-southeast-1.amazonaws.com',
         ],
     },
+    async headers() {
+        return [
+            {
+                // Set the CSP header to allow only the same origin to embed our website in an iframe or a frame.
+                source: '/',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors 'self' https://www.facebook.com https://*.facebook.com",
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
